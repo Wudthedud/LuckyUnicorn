876023b0a07d80b00c2a5c4f793d07f728723b72
Wudthedud <wud@middleton.school.nz>
2023-04-23 18:53:41 +1200

    update

---
+++ Lucky Unicorn V1.py
@@ -0,0 +1,18 @@
+"""
+Lucky Unicorn Version 1
+Welcome Screen
+By Daniel Wu
+"""
+
+
+def instructions():
+    print("============ Welcome to Lucky Unicorn ============= \n"
+          "\t Payout Rewards: \n"
+          "\t Unicorn: 3x Payment \n"
+          "\t Zebra: 1/2 of Payment \n"
+          "\t Horse: 1/2 of Payment \n"
+          "\t Donkey: Nothing \n"
+          "===================================================")
+
+
+instructions()
