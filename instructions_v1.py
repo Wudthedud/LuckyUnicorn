39807901f1d5903507ef1e4f19bb25d8e9d18503
Wudthedud <wud@middleton.school.nz>
2023-04-23 19:29:01 +1200

    update

---
+++ instructions_v1.py
@@ -0,0 +1,12 @@
+def instructions():
+    print("============ Welcome to Lucky Unicorn ============= \n"
+          "Price per round: $1 \n"
+          "\t Payout Rewards: \n"
+          "\t Unicorn: $5 \n"
+          "\t Zebra: $0.5 \n"
+          "\t Horse: $0.5 \n"
+          "\t Donkey: $0 \n"
+          "===================================================")
+
+
+instructions()
