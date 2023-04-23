1d6302869a07681bedc22c3a414be66dc3ea9a5b
Wudthedud <wud@middleton.school.nz>
2023-04-23 19:40:51 +1200

    .

---
+++ num_check_v1.py
@@ -0,0 +1,8 @@
+def number_checker(num):
+    if 1 <= num <= 10:
+        return True
+    else:
+        return False
+
+
+print(number_checker("Choose a number between 1-10"))
---
+++ num_check_v3.py
@@ -0,0 +1,14 @@
+def number_checker():
+    while True:
+        num = input("Please enter a number between 1 and 10: ")
+        if num.isdigit():
+            num = int(num)
+            if 1 <= num <= 10:
+                return num
+            else:
+                print("The number is not between 1 and 10. Please try again.")
+        else:
+            print("Invalid input. Please enter a valid number.")
+
+
+print(number_checker())
