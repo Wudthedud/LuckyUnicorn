7d70bd71805687f188e6c379757e7e0bcffa3131
Wudthedud <wud@middleton.school.nz>
2023-04-23 20:34:47 +1200

    .

---
+++ test.py
@@ -0,0 +1,11 @@
+
+symbol = "*"
+text = "Hello World"
+sides = symbol * 3
+
+formatted_text = f"{sides} {text} {sides}"
+
+top_bottom = symbol * len(formatted_text)
+print(top_bottom)
+print(formatted_text)
+print(top_bottom)
