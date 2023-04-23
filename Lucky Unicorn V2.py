1c16324710684ede1f69f3e9ab412f179d3b53da
Wudthedud <wud@middleton.school.nz>
2023-04-23 19:04:34 +1200

    Update Lucky Unicorn V2.py

--- Lucky Unicorn V2.py
+++ Lucky Unicorn V2.py
@@ -7,10 +7,10 @@ By Daniel Wu
 
 def yes_no(question):
     while True:
-        answer = input(question + "(Y/N").strip().lower()
-        if answer == "yes":
+        answer = input(question + " (Y/N): ").strip().lower()
+        if answer == "yes" or answer == "y":
             return True
-        elif answer == "no":
+        elif answer == "no" or answer == "n":
             return False
 
 
