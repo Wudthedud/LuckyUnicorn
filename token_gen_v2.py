fb41302b18729e56dfdb5330baa2ebb1df78b2ce
Wudthedud <wud@middleton.school.nz>
2023-04-23 19:54:04 +1200

    Update token_gen_v2.py

--- token_gen_v2.py
+++ token_gen_v2.py
@@ -3,18 +3,22 @@ import random
 
 def generate():
     token_name = ""
+    price = 0
     token_list = ["U", "Z", "H", "D"]
 
     token = random.choice(token_list)
     if token == "U":
-        token_name = "Unicorn"
+        token_name = "Unicorn Token"
+        prize = 3
     elif token == "Z":
         token_name = "Zebra Token"
+        prize = 0.5
     elif token == "H":
         token_name = "Horse Token"
+        prize = 0.5
     else:
         token_name = "Donkey Token"
-
+        prize = 0
 
 
 print(generate())
