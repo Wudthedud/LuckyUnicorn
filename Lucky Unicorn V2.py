689b36801ee736a0ec31e12ac13311fc5bd5831b
Wudthedud <wud@middleton.school.nz>
2023-04-23 19:00:47 +1200

    Update Lucky Unicorn V2.py

--- Lucky Unicorn V2.py
+++ Lucky Unicorn V2.py
@@ -1,17 +1,27 @@
 """
-Lucky Unicorn Version 1
-Welcome Screen
+Lucky Unicorn Version 2
+Yes/No Checker
 By Daniel Wu
 """
 
 
+def yes_no(question):
+    while True:
+        answer = input(question + "(Y/N")
+        if answer == "yes":
+            return True
+        elif answer == "no":
+            return False
+
+
 def instructions():
     print("============ Welcome to Lucky Unicorn ============= \n"
+          "Price per round: $1 \n"
           "\t Payout Rewards: \n"
-          "\t Unicorn: 3x Payment \n"
-          "\t Zebra: 1/2 of Payment \n"
-          "\t Horse: 1/2 of Payment \n"
-          "\t Donkey: Nothing \n"
+          "\t Unicorn: $5 \n"
+          "\t Zebra: $0.5 \n"
+          "\t Horse: $0.5 \n"
+          "\t Donkey: $0 \n"
           "===================================================")
 
 
